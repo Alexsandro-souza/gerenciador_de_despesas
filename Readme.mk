c809e0cdd9ff169e267c0a5dd0058b48287a340e
Alexsandro souza <alexsandrosouzasilva68@gmail.com>
2023-06-27 13:40:00 -0300

    feat: Estruturando boilerplate do projeto

---
+++ Readme.mk
@@ -0,0 +1,13 @@
+#Gerenciamento De Despesas#
+
+##Objetivo do projeto##
+
+<p>A ideia é que os usuários possam adicionar despesas, informando detalhes como descrição, valor e categoria. O aplicativo irá armazenar essas informações e exibir uma lista das despesas registradas. Cada despesa será exibida com suas informações correspondentes, como descrição, valor e categoria.
+</p>
+<p>
+O projeto também sugere a implementação de funcionalidades extras, como a exclusão de despesas e a capacidade de categorizá-las. Os usuários podem selecionar uma categoria para cada despesa, permitindo uma melhor organização e análise dos gastos.
+</p>
+<p>
+Além disso, o aplicativo pode calcular e exibir o total de despesas registradas, bem como o saldo disponível com base nas despesas e em um orçamento definido pelos usuários. Isso oferece uma visão geral das finanças pessoais e ajuda os usuários a manter o controle de seus gastos.
+</p>
+
