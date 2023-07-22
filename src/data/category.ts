f1248c36527c2891545e58e3f7ea5cc3abe16766
Alexsandro souza <alexsandrosouzasilva68@gmail.com>
2023-07-21 21:10:03 -0300

    feat: montando lógica para atribuir nova categoria

--- src/data/category.ts
+++ src/data/category.ts
@@ -1,8 +1,8 @@
 import {typesCategory} from './tipagem/typesCategory';
 
-export const category : typesCategory = {
-    food : {title : 'Alimentação', color : 'blue', expense : true},
-    fixed: {title: 'Despesas fixas', color: 'gray', expense: true},
-    entertainment: {title: 'Entreterimento', color: 'purple', expense: true},
-    salary : {title : 'Salário', color : 'green', expense : false},
-}
+export const category : typesCategory = [
+    {title : 'Alimentação', color : 'blue', expense : true},
+    {title: 'Despesas fixas', color: 'gray', expense: true},
+    {title: 'Entreterimento', color: 'purple', expense: true},
+    {title : 'Salário', color : 'green', expense : false},
+]
