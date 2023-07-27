63efc9a185f78591933096728fd67e5521022672
Alexsandro Souza <115429488+Alexsandro-souza@users.noreply.github.com>
2023-07-27 16:34:06 -0300

    Update Readme.mk

--- Readme.mk
+++ Readme.mk
@@ -1,13 +1,5 @@
-#Gerenciamento De Despesas#
-
-##Objetivo do projeto##
-
-<p>A ideia é que os usuários possam adicionar despesas, informando detalhes como descrição, valor e categoria. O aplicativo irá armazenar essas informações e exibir uma lista das despesas registradas. Cada despesa será exibida com suas informações correspondentes, como descrição, valor e categoria.
-</p>
-<p>
-O projeto também sugere a implementação de funcionalidades extras, como a exclusão de despesas e a capacidade de categorizá-las. Os usuários podem selecionar uma categoria para cada despesa, permitindo uma melhor organização e análise dos gastos.
-</p>
-<p>
+# Gerenciamento de Despesas #
+## Objetivo do Projeto ##
+A proposta é permitir que os usuários adicionem despesas, fornecendo detalhes como descrição, valor e categoria. O aplicativo armazenará essas informações e exibirá uma lista das despesas registradas, cada uma com suas informações correspondentes, como descrição, valor e categoria.
+O projeto também sugere a implementação de funcionalidades extras, como exclusão de despesas e a capacidade de categorizá-las. Os usuários podem selecionar uma categoria para cada despesa, permitindo melhor organização e análise dos gastos.
 Além disso, o aplicativo pode calcular e exibir o total de despesas registradas, bem como o saldo disponível com base nas despesas e em um orçamento definido pelos usuários. Isso oferece uma visão geral das finanças pessoais e ajuda os usuários a manter o controle de seus gastos.
-</p>
-
